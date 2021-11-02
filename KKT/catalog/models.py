de6c2c2c69718971832d93dbafa386507a2218e8
casper-man <casper-man@yandex.ru>
2021-11-03 02:25:15 +0300

    #models

--- KKT/catalog/models.py
+++ KKT/catalog/models.py
@@ -2,57 +2,58 @@ from django.db import models
 
 # Create your models here.
 
-class CatalogModel(models.Model):
-    #Fields
-    id = models.AutoField(primary_key=True)
-    id_kkt = models.IntegerField()
-    id_client = models.IntegerField()
-    id_ofd = models.IntegerField()
-    id_fn = models.IntegerField()
 
 class FnModel(models.Model):
     #Fields
-    id = models.AutoField(primary_key=True)
     ffd = models.CharField(max_length=4)
     fh_type = models.CharField(max_length=10)
     fn_z_nom = models.IntegerField(max_length=16)
-    fn_status
-    fn_data_start
-    fn_data_end
-
-
-class KktModel(models.Model):
-    # Fields
-    id = models.AutoField(primary_key=True)
-    rmk
-    id_client
-    id_ofd
-    date_reg
-    kkt_model
-    kkt_z_nom
-    id_fn
+    fn_status = models.BooleanField(default=True)
+    fn_data_start = models.DateField(auto_now_add=True)
+    fn_data_end = models.DateField(default=None)
 
 
 class OfdModel(models.Model):
     # Fields
-    id = models.AutoField(primary_key=True)
-    ofd_inn
-    ofd_full_name
-    ofd_short_name
-    ofd_url
-    ofd_set_addres
-    ofd_set_port
-    ofd_set_email
+    ofd_inn = models.CharField(max_length=12)
+    ofd_full_name = models.CharField(max_length=250)
+    ofd_short_name = models.CharField(max_length=150)
+    ofd_url = models.CharField(max_length=50)
+    ofd_set_address = models.CharField(max_length=50)
+    ofd_set_port = models.CharField(max_length=6)
+    ofd_set_email = models.CharField(max_length=50)
 
 
 class ClientsModel(models.Model):
     # Fields
-    id = models.AutoField(primary_key=True)
-    inn
-    ogrn
-    ip
-    client_full_name
-    client_short_name
-    director_inn
-    predstavitel
-    predstavitel_doc
+    inn = models.CharField(max_length=12)
+    ogrn = models.CharField(max_length=16)
+    ip = models.BooleanField(default=True)
+    client_full_name = models.CharField(max_length=250)
+    client_short_name = models.CharField(max_length=150)
+    director_inn = models.CharField(max_length=12)
+    agent = models.CharField(max_length=150)
+    agent_doc = models.CharField(max_length=150)
+
+
+class KktModel(models.Model):
+    # Fields
+    rmk = models.CharField(max_length=16)
+    id_client = models.ForeignKey(ClientsModel, on_delete=models.PROTECT)
+    id_ofd = models.ForeignKey(OfdModel, on_delete=models.PROTECT)
+    date_reg = models.DateField(auto_now_add=True)
+    kkt_model = models.CharField(max_length=150)
+    kkt_z_nom = models.CharField(max_length=150)
+    id_fn = models.ForeignKey(FnModel, on_delete=models.PROTECT)
+
+
+class CatalogModel(models.Model):
+    #Fields
+    id_kkt = models.ForeignKey(KktModel, on_delete=models.PROTECT)
+    id_client = models.ForeignKey(ClientsModel, on_delete=models.PROTECT)
+    id_ofd = models.ForeignKey(OfdModel, on_delete=models.PROTECT)
+    id_fn = models.ForeignKey(FnModel, on_delete=models.PROTECT)
+    date = models.DateField(auto_now_add=True)
+    agent_status = models.BooleanField(default=False)
+    registration = models.BooleanField(default=False)
+    change_fn = models.BooleanField(default=False)
